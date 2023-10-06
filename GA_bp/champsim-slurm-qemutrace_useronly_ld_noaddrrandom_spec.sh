0d27c1cff2ff034bfe37077438a6d60b961b44df
xkf15 <13051775153@163.com>
2023-10-05 21:14:00 -0400

    Add scripts for submitting spec

--- GA_bp/champsim-slurm-qemutrace_useronly_ld_noaddrrandom_spec.sh
+++ GA_bp/champsim-slurm-qemutrace_useronly_ld_noaddrrandom_spec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=GA_bp_kx      # create a short name for your job
+#SBATCH --job-name=LD_bp_kx      # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
--- GA_bp/submit_iter_noaddrrandom_spec.sh
+++ GA_bp/submit_iter_noaddrrandom_spec.sh
@@ -3,6 +3,10 @@
 # $3 trace1
 # $4 trace2
 
-TRACE1=/scratch/gpfs/kaifengx/trace_br_20230925_600.perlbench_s.out
-TRACE2=/scratch/gpfs/kaifengx/trace_br_20230926_195210_600.perlbench_s.out
-sbatch champsim-slurm-qemutrace_useronly_iter_noaddrrandom_spec.sh 0 600 $TRACE1 $TRACE2
+# TRACE1=/scratch/gpfs/kaifengx/trace_br_20230925_600.perlbench_s.out
+# TRACE2=/scratch/gpfs/kaifengx/trace_br_20230926_195210_600.perlbench_s.out
+# sbatch champsim-slurm-qemutrace_useronly_iter_noaddrrandom_spec.sh 0 600 $TRACE1 $TRACE2
+
+TRACE1=/scratch/gpfs/kaifengx/trace_br_20230927_172323_602.gcc_s.out
+TRACE2=/scratch/gpfs/kaifengx/trace_br_20230927_223338_602.gcc_s.out
+sbatch champsim-slurm-qemutrace_useronly_iter_noaddrrandom_spec.sh 0 602 $TRACE1 $TRACE2
